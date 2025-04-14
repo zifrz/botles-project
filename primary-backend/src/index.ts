ae9b097362c656f62281992da582e2f6ee841d8a
zyedshamsuddin <zyedshamsuddin@gmail.com>
2025-04-14 12:10:58 +0000

    Added action/available and trigger/available routes to primary-backend

--- primary-backend/src/index.ts
+++ primary-backend/src/index.ts
@@ -2,6 +2,8 @@ import express from "express";
 import { userRouter } from "./router/user";
 import { zapRouter } from "./router/zap";
 import cors from "cors";
+import { triggerRouter } from "./router/trigger";
+import { actionRouter } from "./router/action";
 
 const app = express();
 
@@ -17,5 +19,9 @@ app.use(cors(corsOptions))
 app.use("/api/v1/user", userRouter);
 
 app.use("/api/v1/zap", zapRouter);
+
+app.use("/api/v1/trigger", triggerRouter);
+
+app.use("/api/v1/action", actionRouter);
 
 app.listen(3000);
---
+++ primary-backend/src/router/trigger.ts
@@ -0,0 +1,13 @@
+import { Router } from "express";
+import { prismaClient } from "../db";
+
+const router = Router();
+
+router.get("/available", async (req, res) => {
+    const availableTriggers = await prismaClient.availableTrigger.findMany({});
+    res.json({
+        availableTriggers
+    })
+});
+
+export const triggerRouter = router;
