cc1e95a9cc30a1a7c7dc8f87e3b57ffce077cfbc
zyedshamsuddin <zyedshamsuddin@gmail.com>
2025-04-16 11:14:11 +0000

    Updated Dashboard to show WebHook URLs and images

--- frontend/app/config.ts
+++ frontend/app/config.ts
@@ -1,4 +1,5 @@
 // export const BACKEND_URL = "http://localhost:3000"
 
-// This url is only to be used with Project IDX
+// These url is only to be used with Project IDX
 export const BACKEND_URL = "https://3000-idx-botles-project-1741928938413.cluster-e3wv6awer5h7kvayyfoein2u4a.cloudworkstations.dev"
+export const HOOKS_URL = "https://3001-idx-botles-project-1741928938413.cluster-e3wv6awer5h7kvayyfoein2u4a.cloudworkstations.dev"
