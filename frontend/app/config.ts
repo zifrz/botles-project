5b7b8deda97811f2a553e464fd90e14ee9d9ba2a
zyedshamsuddin <zyedshamsuddin@gmail.com>
2025-05-01 05:39:09 +0000

    changed urls

--- frontend/app/config.ts
+++ frontend/app/config.ts
@@ -1,5 +1,5 @@
 // export const BACKEND_URL = "http://localhost:3000"
 
 // These url is only to be used with Project IDX
-export const BACKEND_URL = "https://3000-idx-botles-project-1741928938413.cluster-e3wv6awer5h7kvayyfoein2u4a.cloudworkstations.dev"
-export const HOOKS_URL = "https://3001-idx-botles-project-1741928938413.cluster-e3wv6awer5h7kvayyfoein2u4a.cloudworkstations.dev"
+export const BACKEND_URL = "https://3000-idx-botles-project-1-1744882517715.cluster-nzwlpk54dvagsxetkvxzbvslyi.cloudworkstations.dev"
+export const HOOKS_URL = "https://3002-idx-botles-project-1-1744882517715.cluster-nzwlpk54dvagsxetkvxzbvslyi.cloudworkstations.dev"
