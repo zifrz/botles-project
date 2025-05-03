ff181b2a6a62a5ede0ae7f76b3c043835fbb4144
zyedshamsuddin <zyedshamsuddin@gmail.com>
2025-05-03 11:11:08 +0000

    fix: processor

--- processor/src/index.ts
+++ processor/src/index.ts
@@ -19,12 +19,13 @@ async function main() {
             where: {},
             take: 10
         })
+        console.log(pendingRows);
 
         producer.send({
             topic: TOPIC_NAME,
             messages: pendingRows.map(r => {
                 return {
-                    value: r.zapRunId
+                    value: JSON.stringify({ zapRunId: r.zapRunId, stage: 0 })
                 }
             })
         })
@@ -36,6 +37,8 @@ async function main() {
                 }
             }
         })
+
+        await new Promise(r => setTimeout(r, 3000));
     }
 }
 
