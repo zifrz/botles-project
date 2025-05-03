d984c98980d40db7ccc764b8fcee343d210b10f7
zyedshamsuddin <zyedshamsuddin@gmail.com>
2025-05-03 12:15:03 +0000

    completed worker; added email, sol send action implementation

---
+++ worker/src/email.ts
@@ -0,0 +1,25 @@
+import nodemailer from "nodemailer";
+// SOL_PRIVATE_KEY=""
+// SMTP_USERNAME=""
+// SMTP_PASSWORD=""
+// SMTP_ENDPOINT
+
+const transport = nodemailer.createTransport({
+    host: process.env.SMTP_ENDPOINT,
+    port: 587,
+    secure: false, // upgrade later with STARTTLS
+    auth: {
+      user: process.env.SMTP_USERNAME,
+      pass: process.env.SMTP_PASSWORD,
+    },
+  });
+
+export async function sendEmail(to: string, body: string) {
+    await transport.sendMail({
+        from: "botles.team@gmail.com",
+        sender: "botles.team@gmail.com",
+        to,
+        subject: "Hello from Botles",
+        text: body
+    })
+}
